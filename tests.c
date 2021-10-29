3d432f51fe35e54544263842f2127e849adc8934
Rolando Gonzalez-Chevere <rgonzalez@hoyosintegrity.com>
2021-10-29 07:50:47 -0400

    - Other ways to use json_get

--- tests.c
+++ tests.c
@@ -137,17 +137,17 @@ char test_json_get() {
   JsonValue* v = json_decode(json, sizeof(json) - 1);
   char r = 1;
 
-  JsonValue* val = json_get(v, "\"a\".\"1\".0.\"b\".1");
+  JsonValue* val = json_get(v, "'{a}.{1}.0.{b}.1");
   r = r && val->type == JSON_NUMBER;
   JsonNumber* n = (JsonNumber*)val->data;
   r = r && n->value == 4;
 
-  val = json_get(v, "\"b\".1.\"q\"");
+  val = json_get(v, "{b}.1.{q}");
   r = r && val->type == JSON_BOOL;
   JsonBool* b = (JsonBool*)val->data;
   r = r && b->value == 1;
 
-  val = json_get(v, "\"b\".1.\"rrr\"");
+  val = json_get(v, "{b}.1.{rrr}");
   r = r && val->type == JSON_NULL;
   JsonNull* nu = (JsonNull*)val->data;
   r = r && nu->value == 0;
