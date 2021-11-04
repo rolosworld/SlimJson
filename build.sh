f63a091a1e3bceb0dee8831b03c8aeee9d5d81a3
Rolando Gonzalez-Chevere <rgonzalez@hoyosintegrity.com>
2021-11-04 07:55:21 -0400

    - Cleanup

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Building decode tests"
 gcc -ggdb slim_json_decode.c test_decode.c -o test_decode
+
+echo "Building encode tests"
 gcc -ggdb slim_json_encode.c test_encode.c -o test_encode
